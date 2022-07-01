154ee4b68f2827122d8736540f128d5836bca5e9
allankimanzi2 <allankim457@gmail.com>
2022-07-01 05:03:58 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main -> assign a random to the variable n each time it is executed
+*and print the last digit of the variable n
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int ch;
+for (ch = 48; ch <= 57; ch++)
+{
+putchar(ch);
+}
+putchar(10); /* this is an ascii code for new line*/
+return (0);
+}
