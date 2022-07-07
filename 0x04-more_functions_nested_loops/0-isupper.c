7dac3c7e7a85100b4c61f037432bd2c43c613097
allankimanzi2 <allankim457@gmail.com>
2022-07-07 03:44:09 -0700

    task 0 more functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+*_isupper - this is a function to check upper class alphas
+*@c: an integer parameter
+*Return: something but success
+*/
+int _isupper(int c)
+{
+return (c >= 'A' && c <= 'Z');
+}
