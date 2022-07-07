e895c6891e0d56cd6672665a9528861b46b1a0f2
allankimanzi2 <allankim457@gmail.com>
2022-07-07 14:19:52 -0700

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,16 @@
 #include "main.h"
 /**
-*print_numbers - print numbers
+*print_numbers - prints 0 to 9 with new line
+*only use _putchar
 */
 void print_numbers(void)
 {
-int x;
-for (x = 0; x < 10; x++)
+int y;
+y = 0;
+while (y < 10)
 {
-_putchar(x + '0');
+_putchar(y + '0');
+y++;
 }
 _putchar('\n');
 }
