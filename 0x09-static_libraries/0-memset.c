511c3be7e3e95d8720623be764d53054b84db9c3
allankimanzi2 <allankim457@gmail.com>
2022-07-22 15:39:20 -0700

    task

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _memset - Entry point
+*@s: pointed destination
+*@b: constant byte
+*@n: bytes
+*Return: Always 0 (Success)
+*/
+char *_memset(char *Is, char b, unsigned int n)
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+s[i] = b;
+return (s);
+}
