40216176ee67e1ee9065e3641136fd002b408acc
allankimanzi2 <allankim457@gmail.com>
2022-07-28 06:16:09 -0700

    task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,7 +4,7 @@
 /**
 *malloc_checked - Allocates memory using malloc.
 *@b: The number of bytes to be allocated.
-*    
+*
 *Return: A pointer to the allocated memory.
 */
 void *malloc_checked(unsigned int b)
