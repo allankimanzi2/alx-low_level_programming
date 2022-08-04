d870c82b031344ad84af080e7b7525856e0c7366
allankimanzi2 <allankim457@gmail.com>
2022-08-04 04:43:09 -0700

    task 3

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,9 +1,6 @@
-#ifndef CALC_H
-#define CALC_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 /**
 *struct op - Struct op
 *
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,33 +1,31 @@
-nclude "3-calc.h"
+#include "3-calc.h"
+#include <stdio.h>
+#include <main.h>
 
 /**
- *  * get_op_func - function pointer that selects the correct function to perform
- *   * the operation asked by the user
- *    * @s: the operator given by the user
- *     *
- *      * Return: pointer to the function that corresponds to the
- *       * operator given as a parameter
- *        */
+*get_op_func - function pointer that selects the correct function to perform
+*the operation asked by the user
+*@s: the operator given by the user
+*
+*Return: pointer to the function that corresponds to the
+*/
 int (*get_op_func(char *s))(int, int)
 {
-		op_t ops[] = {
-					{"+", op_add},
-							{"-", op_sub},
-									{"*", op_mul},
-											{"/", op_div},
-													{"%", op_mod},
-															{NULL, NULL}
-						};
-			int j;
-
-				j = 0;
-
-					while (ops[j].op)
-							{
-										if (strcmp(ops[j].op, s) == 0)
-														return (ops[j].f);
-												j++;
-													}
-
-						return (NULL);
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i;
+i = 0;
+while (i < 5)
+{
+if (ops[i].op[0] == s[0])
+return (ops[i].f);
+i++;
+}
+return (NULL);
 }
