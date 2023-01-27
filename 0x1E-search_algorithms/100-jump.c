2dc2ff7921f33d09442a467a727bc2ff27bc43d1
allankimanzi2 <allankim457@gmail.com>
2023-01-27 00:08:25 -0800

    indentations

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -17,20 +17,19 @@ int jump_search(int *array, size_t size, int value)
 {
 	size_t i, jump, step;
 if (array == NULL || size == 0)
-
-		return (-1);
+return (-1);
 step = sqrt(size);
-	for (i = jump = 0; jump < size && array[jump] < value;)
-	{
-		printf("value checked array[%ld] = [%d]\n", jump, array[jump]);
+for (i = jump = 0; jump < size && array[jump] < value;)
+{
+	printf("value checked array[%ld] = [%d]\n", jump, array[jump]);
 		i = jump;
 		jump += step;
-	}
-	printf("Value found between indexes [%ld] and [%ld]\n", i, jump;
+}
+printf("Value found between indexes [%ld] and [%ld]\n", i, jump;
 jump = jump < size - 1 ? jump : size - 1;
-			for (; i < jump && array[i] < value; i++)
-			printf("value checked array[%ld] = [%d]\n", i, array[i]);
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+for (; i < jump && array[i] < value; i++)
+	printf("value checked array[%ld] = [%d]\n", i, array[i]);
+	printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 return (array[i] == value ? (int)i : -1);
 }
 
